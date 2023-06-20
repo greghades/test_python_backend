02f4de42064d78a01d8db9b7a59c27a880c0da60
gregjose25131 <gregjose2513@gmail.com>
2023-06-20 11:39:51 -0400

    First_commit

---
+++ core/aplications/concesiones_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ConcesionesAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'aplications.concesiones_app'
---
+++ core/aplications/jurisprudencias_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class JurisprudenciasAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'aplications.jurisprudencias_app'
